58b47897a046d44ce607e5928bfc807275a94f91
Tristan Chilvers <tmchilvers@gmail.com>
2022-06-18 11:49:19 -0700

    Added info panel for seed

---
+++ prototypes/justInMind/MH_220618_TCv01/resources/widgets/widgets-description-1655578038421.js
@@ -0,0 +1,146 @@
+	window.widgets = {
+		descriptionMap : widgetDescriptionMap = {},
+		rootWidgetMap : widgetRootMap = {}
+	};
+
+	widgets.descriptionMap[["s-Paragraph", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph"]; 
+
+	widgets.descriptionMap[["s-Button", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button"]; 
+
+	widgets.descriptionMap[["s-Button_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_1"]; 
+
+	widgets.descriptionMap[["s-Input", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input"]; 
+
+	widgets.descriptionMap[["s-Button_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_2"]; 
+
+	widgets.descriptionMap[["s-Line", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line"]; 
+
+	widgets.descriptionMap[["s-Line_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line_2"]; 
+
+	widgets.descriptionMap[["s-Line_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line_1"]; 
+
+	widgets.descriptionMap[["s-Line_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line_3"]; 
+
+	widgets.descriptionMap[["s-Line_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line_4"]; 
+
+	widgets.descriptionMap[["s-Line_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Line_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Line", "s-Line_5"]; 
+
+	widgets.descriptionMap[["s-Paragraph_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_1"]; 
+
+	widgets.descriptionMap[["s-Paragraph_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_2"]; 
+
+	widgets.descriptionMap[["s-Paragraph_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_3"]; 
+
+	widgets.descriptionMap[["s-Ellipse", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Ellipse", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Ellipse", "s-Ellipse"]; 
+
+	widgets.descriptionMap[["s-Ellipse_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Ellipse_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Ellipse", "s-Ellipse_1"]; 
+
+	widgets.descriptionMap[["s-Ellipse_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Ellipse_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Ellipse", "s-Ellipse_2"]; 
+
+	widgets.descriptionMap[["s-Paragraph_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_4"]; 
+
+	widgets.descriptionMap[["s-Paragraph_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_5"]; 
+
+	widgets.descriptionMap[["s-Paragraph_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_6"]; 
+
+	widgets.descriptionMap[["s-Input_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input_3"]; 
+
+	widgets.descriptionMap[["s-Input_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input_4"]; 
+
+	widgets.descriptionMap[["s-Input_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input_5"]; 
+
+	widgets.descriptionMap[["s-Button_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_3"]; 
+
+	widgets.descriptionMap[["s-Input_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input_1"]; 
+
+	widgets.descriptionMap[["s-Input_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Input Text Field", "s-Input_2"]; 
+
+	widgets.descriptionMap[["s-Button_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_4", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_4"]; 
+
+	widgets.descriptionMap[["s-Panel_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Panel_1", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Dynamic Panel", "s-Dynamic_Panel"]; 
+
+	widgets.descriptionMap[["s-Button_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_6"]; 
+
+	widgets.descriptionMap[["s-Button_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_5", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Button", "s-Button_5"]; 
+
+	widgets.descriptionMap[["s-Category", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Category", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Custom Select List", "s-Category"]; 
+
+	widgets.descriptionMap[["s-Panel_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Panel_2", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Dynamic Panel", "s-Dynamic_Panel"]; 
+
+	widgets.descriptionMap[["s-Circle", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Circle", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Circle", "s-Circle"]; 
+
+	widgets.descriptionMap[["s-Paragraph_7", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_7", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Text", "s-Paragraph_7"]; 
+
+	widgets.descriptionMap[["s-Panel_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Panel_3", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Dynamic Panel", "s-Dynamic_Panel_1"]; 
+
+	
