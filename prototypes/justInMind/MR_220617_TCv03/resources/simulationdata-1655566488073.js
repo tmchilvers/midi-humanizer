fbcd7b51717668b18f5cd5a5de59483e1979b0a0
Tristan Chilvers <tmchilvers@gmail.com>
2022-06-18 09:15:26 -0700

    Created first JustInMind prototype

---
+++ prototypes/justInMind/MR_220617_TCv03/resources/simulationdata-1655566488073.js
@@ -0,0 +1,12 @@
+function initData() {
+  jimData.variables["seed_6"] = "6";
+  jimData.variables["seed_2"] = "2";
+  jimData.variables["seed_3"] = "3";
+  jimData.variables["seed_4"] = "4";
+  jimData.variables["seed_5"] = "5";
+  jimData.variables["seed_1"] = "1";
+  jimData.datamasters["Seeds"] = [
+  ];
+
+  jimData.isInitialized = true;
+}
